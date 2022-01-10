2724743a14e23f76f833e56b75ff7d8ee49c5140
Kerem Erguner <73108947+keremerguner@users.noreply.github.com>
2022-01-10 05:01:01 +0300

    v2.0

--- employeeinfo.cpp
+++ employeeinfo.cpp
@@ -28,8 +28,7 @@ void EmployeeInfo::on_pushButton_clicked()
 {
     MainWindow conn;
     //Login Panelinde kullanıcı adi ve sifre line olusturuldu
-    QString roll_id,name,surname,blood_group,tc,gender,age;
-    roll_id=ui->txt_roll_id->text();
+    QString name,surname,blood_group,tc,gender,age;
     name=ui->txt_name->text();
     surname=ui->txt_surname->text();
     blood_group=ui->txt_blood_group->text();
@@ -47,7 +46,7 @@ void EmployeeInfo::on_pushButton_clicked()
 //
     conn.connOpen();
     QSqlQuery qry;
-    qry.prepare("INSERT INTO Users(roll_id,name,surname,blood_group,tc,gender,age) VALUES('"+roll_id+"', '"+name+"', '"+surname+"','"+blood_group+"','"+tc+"','"+gender+"','"+age+"')");
+    qry.prepare("INSERT INTO Users(name,surname,blood_group,tc,gender,age) VALUES('"+name+"', '"+surname+"','"+blood_group+"','"+tc+"','"+gender+"','"+age+"')");
 
     if(qry.exec()){
     QMessageBox::critical(this,tr("Save"),tr("Saved"));
@@ -65,8 +64,7 @@ void EmployeeInfo::on_pushButton_Edit_clicked()
 
         MainWindow conn;
         //Login Panelinde kullanıcı adi ve sifre line olusturuldu
-        QString roll_id,name,surname,blood_group,tc,gender,age;
-        roll_id=ui->txt_roll_id->text();
+        QString name,surname,blood_group,tc,gender,age;
         name=ui->txt_name->text();
         surname=ui->txt_surname->text();
         blood_group=ui->txt_blood_group->text();
@@ -85,7 +83,7 @@ void EmployeeInfo::on_pushButton_Edit_clicked()
         conn.connOpen();
         QSqlQuery qry;
         //En sonda where diye eklenen yer nereyi parametre alarak duzenleme yapacagini belirtir. Biz roll_id'ye göre update islemini gerceklestiriyoruz
-        qry.prepare("update Users set roll_id='"+roll_id+"',name='"+name+"',surname='"+surname+"',blood_group='"+blood_group+"',tc='"+tc+"',gender='"+gender+"',age='"+age+"' where roll_id='"+roll_id+"'");
+        qry.prepare("update Users set name='"+name+"',surname='"+surname+"',blood_group='"+blood_group+"',tc='"+tc+"',gender='"+gender+"',age='"+age+"' where tc='"+tc+"'");
 
         if(qry.exec()){
         QMessageBox::critical(this,tr("Edit"),tr("Update"));
@@ -104,9 +102,9 @@ void EmployeeInfo::on_pushButton_Delete_clicked()
     //Delete isleminde sadece roll idye gore yapacagimiz icin onun disindakiler yorumda
     MainWindow conn;
     //Login Panelinde kullanıcı adi ve sifre line olusturuldu
-    QString roll_id;
-    //QString name,surname,blood_group,tc,gender,age
-    roll_id=ui->txt_roll_id->text();
+    QString tc;
+    //QString name,surname,blood_group,gender,age
+    tc=ui->txt_tc->text();
     //name=ui->txt_name->text();
     //surname=ui->txt_surname->text();
     //blood_group=ui->txt_blood_group->text();
@@ -124,7 +122,7 @@ void EmployeeInfo::on_pushButton_Delete_clicked()
 //
     conn.connOpen();
     QSqlQuery qry;
-    qry.prepare("Delete from Users where roll_id='"+roll_id+"'");
+    qry.prepare("Delete from Users where tc='"+tc+"'");
 
     if(qry.exec()){
     QMessageBox::critical(this,tr("Delete"),tr("Deleted"));
@@ -144,13 +142,52 @@ void EmployeeInfo::on_pushButton_load_tbl_clicked()
     conn.connOpen();
     QSqlQuery* qry = new QSqlQuery(conn.mydb);
 
-    qry->prepare("select name,surname,blood_group from Users");
+    qry->prepare("select name,surname,blood_group,tc from Users");
 
     qry->exec();
     modal->setQuery(*qry);
     ui->tableView->setModel(modal);
+    ui->listView->setModel(modal);
+
 
     conn.connClose();
     qDebug() << (modal->rowCount());
 }
 
+
+void EmployeeInfo::on_listView_activated(const QModelIndex &index)
+{
+    QString val=ui->listView->model()->data(index).toString();
+
+    MainWindow conn;
+
+    //Database icine giris yapildi mi kontrolu saglandi (sorgulari yapildi)
+    if(!conn.connOpen()){
+        qDebug()<< "Failed to open the database";
+        return;
+
+    }
+//
+    conn.connOpen();
+    QSqlQuery qry;
+    qry.prepare("select * from Users where name='"+val+"'");
+
+    if(qry.exec()){
+
+        while(qry.next()){
+            ui->txt_tc->setText(qry.value(7).toString());
+            ui->txt_name->setText(qry.value(4).toString());
+            ui->txt_surname->setText(qry.value(5).toString());
+            ui->txt_blood_group->setText(qry.value(6).toString());
+            ui->txt_gender->setText(qry.value(8).toString());
+            ui->txt_age->setText(qry.value(9).toString());
+
+
+        }
+
+    conn.connClose();
+    }
+    else{
+        QMessageBox::critical(this,tr("error::"),qry.lastError().text());
+    }
+}
